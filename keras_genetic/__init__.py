4c02b40559fdb9b841e56490a3db19b364abbf23
Luke <lukewoodcs@gmail.com>
2022-03-19 16:28:23 -0700

    Ignore build, add version infor

--- keras_genetic/__init__.py
+++ keras_genetic/__init__.py
@@ -2,3 +2,5 @@ from keras_genetic.breeder import Breeder
 from keras_genetic.core.individual import Individual
 from keras_genetic.initializers import Initializer
 from keras_genetic.search import search
+
+__version__ = '0.0.1'
