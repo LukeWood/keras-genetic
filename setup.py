e643094564bf917432afe54d02957726f6f533b7
Luke <lukewoodcs@gmail.com>
2022-03-19 17:30:54 -0700

    remove version from setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ README = (HERE / "README.md").read_text()
 setup(
     name="keras-genetic",
     description="Train keras models with genetic algorithms.",
-    version="0.0.1",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/lukewood/keras-genetic",
