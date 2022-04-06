cac906e4d0a8a777fbc2c9484c144c226fe796f9
Luke Wood <lukewoodcs@gmail.com>
2022-04-05 19:47:38 -0700

    Remove old import

--- keras_genetic/breeder/__init__.py
+++ keras_genetic/breeder/__init__.py
@@ -1,4 +1,3 @@
 from keras_genetic.breeder.breeder import Breeder
-from keras_genetic.breeder.cma_breeder import CMABreeder
 from keras_genetic.breeder.mutation_breeder import MutationBreeder
 from keras_genetic.breeder.random_weight_breeder import RandomWeightBreeder
